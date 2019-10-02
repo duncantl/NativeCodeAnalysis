be7ad2885cd3d9f19cfb82c6e03ecb6f81065675
Duncan Temple Lang <duncan@r-project.org>
2019-10-01 19:06:38 -0700

    getReturnValues() function

---
+++ R/getReturnValues.R
@@ -0,0 +1,12 @@
+
+getReturnValues =
+  # This returns the LLVM Value objects     
+function(fun, blocks = getBlocks(fun))
+{
+
+    terms = lapply(blocks, getTerminator, FALSE)
+    isRet = sapply(terms, is, 'ReturnInst')
+    rets = terms[isRet]
+    lapply(rets, `[[`, 1L)
+}
+
