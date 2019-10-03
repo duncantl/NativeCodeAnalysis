fcf85a21a7a2c5903468fde7e3a969f6a2d42731
Nick Ulle <nick.ulle@gmail.com>
2019-10-02 20:39:57 -0700

    More notes on convolve example.

--- examples/convolve/analyze.R
+++ examples/convolve/analyze.R
@@ -187,6 +187,142 @@ function(inst)
 #
 # TODO: Fix it tomorrow.
 
+# ------------------------------------------------------------
+# 2019-10-02
+#
+# Per DTL above, if we compile with some level of optimization:
+#
+#     clang -fno-discard-value-names -O1 -S -emit-llvm convolve.c
+#
+# Then the IR is annotated with `readonly` on parameters that are read only.
+# The Rllvm function `onlyReadsMemory()` checks for this annotation.
+m = parseIR("convolve.ll")
+
+params = getParameters(m$convolve)
+is_input_only = lapply(params, onlyReadsMemory)
+
+# TODO: So now we can build a function to get the parameter types and whether
+# they are inputs/outputs.
+#
+# The `onlyReadsMemory()` function does not tell us whether the outputs are
+# read, but that information is less important from an R-user perspective.
+#
+# The next thing we'd like to do is determine the (symbolic) dimensions of the
+# parameters.
+#
+# We can determine whether the parameters are vectors or scalars based on how
+# they are accessed. In other words, we'll assume that a parameter is a scalar
+# if only its first element is accessed; all other parameters are vectors.
+# 
+# In LLVM IR, element access is a two step process. The first step is to get
+# the address of the element with `getelementptr`. For scalars, this step is
+# not necessary because pointers point to the first element. The second step is
+# to load the element into a register with `load`.
+#
+# Clang does not generate a `getelementptr` for a dereference operation `*x`,
+# and let's assume it won't for the equivalent `x[0]` either. So
+# `getelementptr` will only be generated to read/write vectors.
+#
+# We can traverse all of the blocks to find all of the `getelementptr`
+# operations, and then check whether they affect the parameters. Alternatively,
+# we can follow each parameter through the code to see if it is ever subject to
+# `getelementptr`.
+#
+# In order to make this robust, we need to deal with aliases created by
+# load/store. In other words, we need the use-def chain.
+#
+# However, we can still try the strategy without the complete use-def chains to
+# get an approximation.
+
+blocks = getBlocks(m$convolve)
+
+findOps =
+function(x, test, ...)
+# Find all ops that satisfy a condition.
+{
+  if (is(x, "Function"))
+    x = getBlocks(x)
+
+  found = lapply(x, function(block) {
+    # QQ: Might be nice if getBlockInstructions was an as.list method so that
+    # getBlockInstructions isn't necessary here.
+    ops = getBlockInstructions(block)
+    is_match = vapply(ops, test, NA, ...)
+    ops[is_match]
+  })
+  unlist(found, recursive = FALSE, use.names = FALSE)
+}
+
+geps = findOps(m$convolve, is, "GetElementPtrInst")
+vecs = sapply(geps, function(op) {
+  getName(getOperand(op, 1))
+})
+
+# So now we can conclude that a, b, ab are vectors.
+#
+# We can also conclude that the remaining parameters
+setdiff(params@names, vecs)
+# na, nb are scalars, since there are only 3 gep instructions in the code. If
+# there were more gep instructions, we would need to check for aliasing to be
+# sure.
+#
+# TODO: We've demonstrated everything necessary to build a function that
+# approximates which parameters are vectors and which are scalars.
+#
+# The next step is to try to recognize that `*na` and `*nb` are the lengths of
+# `a` and `b`, respectively. We'd also like to recognize that the length of
+# `ab` is `*na + *nb - 1`.
+#
+# Let's start by looking at how to figure this out manually from the original C
+# code. The key is the loops.
+#
+# The loop `for(int i = 0; i < nab; i++)` iterates from 0 to `nab - 1`. The loop
+# variable `i` is used to access elements of `ab`. So we can conclude that `ab`
+# has length `nab`. By working backwards through the code, we can see that
+# `nab` is equal to `*na + *nb - 1`.
+#
+# The loop `for(int i = 0; i < *na; i++)` iterates from 0 to `*na - 1`. The
+# loop variable `i` is used to access elements of `a`. So `a` has length at
+# least `*na`.
+#
+# The loop `for(int j = 0; j < *nb; j++)` iterates from 0 to `*nb - 1`. The
+# loop variable `j` is used to access elements of `b`. So `b` has length at
+# least `*nb`.
+#
+# The loop variables `i` and `j` from the latter two loops are also used in the
+# sum `i + j` to access elements of `ab`. Since `i` and `j` are non-negative,
+# we know that `ab` must have length at least `*na + *nb - 1` (the `-1` is
+# because the first element is 0).
+#
+# All of this assumes that the loop variables are never reassigned in the
+# middle of the loop. Reassignment is possible, but unlikely unless the user is
+# a relatively sophisticated C programmer (most R users are not). So it is
+# probably safe to assume that the loop variables are not reassigned.
+#
+# Similarly, this assumes that `na` and `nb` are not reassigned. We can check
+# this with the `readonly` attribute discussed above.
+#
+# With -O1 optimization, the first loop is completely replaced in the code by a
+# call to the `llvm.memset.*` intrinsic routine. The `llvm.memset.*` intrinsic
+# fills a block of memory with a particular byte value. The third argument to
+# the intrinsic is the length (number of bytes) to fill.
+#
+# Since the number of bytes is needed, the LLVM IR must multiply the length by
+# 8. The IR does this with a `shl` instruction:
+#
+#     %2 = add i32 %1, %0           ; %2 = na + nb
+#     %3 = add i32 %2, -2           ; %3 = na + nb - 2
+#     %4 = zext i32 %3 to i64       ; zero extend to i64
+#     %5 = shl nuw nsw i64 %4, 3    ; %5 = (na + nb - 2) * 2^3
+#     %6 = add nuw nsw i64 %5, 8    ; %6 = (na + nb - 2) * 8 + 8
+#                                   ;    = (na + nb - 1) * 8
+#
+#     call void @llvm.memset.p0i8.i64(i8* align 8 %ab55, i8 0, i64 %6, i1 false)
+#
+# It's not clear why the Clang subtracts 2 only to add back 8 bytes later.
+#
+# Since `ab` is a double, shouldn't there be more bytes to fill?
+
 
 # References:
 #   * Rllvm/AnalyzeCCode/eg.R
