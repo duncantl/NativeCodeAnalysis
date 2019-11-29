427b7d60fc7ba03287e4209ebad642a4302248ba
Duncan Temple Lang <duncan@r-project.org>
2019-11-29 09:12:53 -0800

    code to create the call graph from LLVM modules

--- R/callGraph.R
+++ R/callGraph.R
@@ -35,16 +35,42 @@ setMethod("getCalledRoutines", "Instruction",
               
 
 library(igraph)
+
 setGeneric("callGraph",
            function(x, ...)
-             standardGeneric("callGraph"))
+              standardGeneric("callGraph"))
+
+
+
+setMethod("callGraph", "character",
+          function(x, pattern = "\\.(ll|ir)", ...) {
+              # if is a file name, parse the module
+              # if a directory, parse all the IR modules
+              # if a mixture of file names and directories, merge the file names from the directories into the full list of file names.
+
+              e = file.exists(x)
+              if(any(!e))
+                  stop("files don't exist", paste(x[!e], collapse = ", "))
+              
+              info = file.info(e)
+              pdir = info$isdir
+              if(any(pdir)) {
+                  tmp = unlist(lapply(x[pdir], list.files, pattern = patter, full.names = TRUE))
+                  x = c(x[!pdir], tmp)
+              }
+              
+              mods = lapply(x, parseIR)
+              callGraph(structure(mods, class = "ListOfModules"))
+          })
+
 
 
 setMethod("callGraph", "Module",
-          function(x, ...) {
+          function(x, within = TRUE, ...) {
               funs = getDefinedRoutines(, x)
               calls = lapply(funs, function(x) getCalledRoutines(sh[[x]]))
               names(calls) = funs
+               # NAs are for calls via routine pointers.
               tmp = lapply(tmp, function(x) unique(x[!is.na(x)]) )
               d = data.frame(from = rep(names(tmp), sapply(tmp, length)), to = unlist(tmp), stringsAsFactors = FALSE)
               igraph::graph_from_data_frame(d)    
