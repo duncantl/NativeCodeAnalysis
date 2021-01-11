e808420107642389d2246ca02916372f0998e278
Duncan Temple Lang <duncan@r-project.org>
2021-01-11 07:36:35 -0800

    exports and R code for them

--- R/classHierarchy.R
+++ R/classHierarchy.R
@@ -3,8 +3,8 @@
 #  list(className = subClasses)
 #
 #
-getClassHierarchy =
-function(baseType, allClasses, fixNames = function(x) sprintf("llvm::%s", x), recursive = TRUE)
+getSubclasses = 
+function(baseType, allClasses, fixNames = function(x) sprintf("llvm::%s", x), recursive = TRUE, verbose = TRUE)
 {
     k = allClasses
     isSub = sapply(k, function(x) baseType %in% names(x@superClasses))
@@ -12,17 +12,19 @@ function(baseType, allClasses, fixNames = function(x) sprintf("llvm::%s", x), re
     if(!any(isSub))
        return(baseType)
     
-    print(baseType)#;browser()
+    if(verbose)
+        message(baseType)
     tmp = fixNames(names(k[isSub]))
     ans = structure(as.list(tmp), names = tmp)
     if(recursive && any(isSub)) {
        # tmp = fixNames( names(k)[isSub])
-       v = lapply(tmp, getClassHierarchy, allClasses, fixNames)
+       v = lapply(tmp, getSubclasses, allClasses, fixNames)
        names(v) = tmp
        w = sapply(v, function(x) length(x) > 0 && !is.character(x) ) # || length(x[[1]]) > 0)
        ans = c(v[w], ans)
 #       ans[w] = v[w]
-     }
+    } else
+        return(unname(unlist(ans)))
      ans = structure(list(ans), names = baseType)
 
      ans
--- R/getIRCalls.R
+++ R/getIRCalls.R
@@ -13,20 +13,21 @@ setGeneric("getCalls", function(x, ...) standardGeneric("getCalls"))
 setMethod("getCalls", "Module",
           function(x, names = TRUE, ...) {
               r = getDefinedRoutines(module = x, names = FALSE)
-              ins = unlist(lapply(r, getInstructions))
-              w = sapply(ins, is, "CallInst")
-              if(!names)
-                  return(ins[w])
-              
-              ids = sapply(ins[w], function(x) getName(getCalledFunction(x)))
+              ins = unlist(lapply(r, getCalls, names, ...))
           })
 
 setMethod("getCalls", "Function",
           function(x, names = TRUE, ...) {
-              ins = unlist(lapply(r, getInstructions))
+              ins = getInstructions(x)
+              getCalls(ins, names = names, ...)
+          })
+
+setMethod("getCalls", "list",
+          function(x, names = TRUE, ...) {
+              ins = x
               w = sapply(ins, is, "CallInst")
               if(!names)
                   return(ins[w])
               
-              ids = sapply(ins[w], function(x) getName(getCalledFunction(x)))              
+              sapply(ins[w], function(x) getName(getCalledFunction(x)))              
           })
