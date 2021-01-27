bd7a656dedfa0c9ba5730c8ba265649be551f48a
Duncan Temple Lang <duncan@r-project.org>
2021-01-27 09:56:34 -0800

    handle void return types

--- R/getReturnValues.R
+++ R/getReturnValues.R
@@ -3,6 +3,9 @@ getReturnValues =
   # This returns the LLVM Value objects     
 function(fun, blocks = getBlocks(fun))
 {
+    if(sameType(getReturnType(fun), VoidType))
+        return(NULL)
+    
     terms = lapply(blocks, getTerminator, FALSE)
     isRet = sapply(terms, is, 'ReturnInst')
     rets = terms[isRet]
