b2474a437bfd1e286227ac25bbd5f5a843c9d32b
Duncan Temple Lang <duncan@r-project.org>
2021-02-11 11:58:13 -0800

    oop detection functions

--- CRAN/funs.R
+++ CRAN/funs.R
@@ -7,3 +7,35 @@ function(d)
    any(grepl("Rcpp", unlist(dc[, m])))
 }
 
+
+
+getPkgName =
+function(path, base = "./",  strip = TRUE)
+{
+    if(strip)
+        path = gsub("^([^:]+):.*", "\\1", path)
+
+    base = path.expand(base)
+    if(!grepl("/$", base))
+        base = paste0(base, "/")
+    
+    tmp = gsub(base, "", path, fixed = TRUE)
+    sapply(strsplit(tmp, "/"), `[`, 1L)
+}
+
+
+
+usesMethods =
+function(pkg, desc = read.dcf(file.path(pkg, "DESCRIPTION")))
+{
+    m = match(c("Imports", "Depends"), colnames(desc), 0)
+    any(grepl("methods", unlist(desc[1, m])))
+}
+
+s3Methods =
+function(pkg, ns = readLines(file.path(pkg, "NAMESPACE"), warn = FALSE))
+{
+   length(grep("S3method", ns))
+}
+
+
