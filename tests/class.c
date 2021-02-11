232378236b6ef6857de337425699117d9ebd64f4
Duncan Temple Lang <duncan@r-project.org>
2021-02-11 11:50:25 -0800

    additional examples where we set the class of an R object in C code

--- tests/class.c
+++ tests/class.c
@@ -1,4 +1,3 @@
-
 #include <Rdefines.h>
 
 SEXP
@@ -34,4 +33,74 @@ doS4()
     SEXP k = R_do_MAKE_CLASS("Bob");
     SEXP obj = R_do_new_object(k);
     return(obj);
+}
+
+
+SEXP
+listEls(SEXP a)
+{
+    SEXP ans;
+    PROTECT(ans = NEW_LIST(2));
+    SET_VECTOR_ELT(ans, 0, doS4());
+    SET_VECTOR_ELT(ans, 1, mk2(a));    
+    UNPROTECT(1);
+    return(ans);
+}
+
+
+SEXP
+setElNames(SEXP ans)
+{
+    SEXP names;
+    PROTECT(ans);
+    PROTECT(names = NEW_CHARACTER(2));
+    SET_STRING_ELT(names, 0, mkChar("A"));
+    SET_STRING_ELT(names, 1, mkChar("B"));    
+    SET_NAMES(ans, names);
+    UNPROTECT(2);
+    return(ans);
+}
+
+
+SEXP
+listEls1(SEXP a)
+{
+    SEXP ans = listEls(a);
+    return(setElNames(a));
+}
+
+
+SEXP
+listEls2(SEXP a)
+{
+    SEXP ans;
+    PROTECT(ans = NEW_LIST(2));
+    SET_VECTOR_ELT(ans, 0, doS4());
+    SET_VECTOR_ELT(ans, 1, mk2(a));
+    Rf_setAttrib(ans, Rf_install("class"), ScalarString(mkChar("MyList")));
+    UNPROTECT(1);
+    return(ans);
+}
+
+/*
+ This one calls listEls2 and then passes the result to setElNames
+ which merely adds to the one parameter and returns it. 
+ */
+SEXP
+listEls3(SEXP a)
+{
+    return(setElNames(listEls2(a)));
+}
+
+
+const char *mkClassName(SEXP x);
+
+SEXP
+mkUnknown(SEXP x)
+{
+    SEXP y = NEW_NUMERIC(Rf_length(x));
+    PROTECT(y);
+    Rf_setAttrib(y, R_ClassSymbol, ScalarString(mkChar(mkClassName(x))));
+    UNPROTECT(1);
+    return(y);
 }
