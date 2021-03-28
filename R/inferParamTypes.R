238ce223da4bc1a398955813bfd0a632a9a6635d
Duncan Temple Lang <duncan@r-project.org>
2021-03-27 17:47:00 -0700

    committing this as getTypeOf may need to be merged with the newly added testType() and typeofSwitch.R

--- R/inferParamTypes.R
+++ R/inferParamTypes.R
@@ -1,4 +1,7 @@
 inferParamTypes =
+    #
+    #
+    #
 function(fun, params = getParameters(fun))
 {
    lapply(params, inferParamType)
@@ -11,13 +14,24 @@ inferParamType =
     #   only used as a scalar. E.g. STRING_ELT(p, 0) or STRING_ELT(p, i).
     #   The C code could use asChar, asReal, etc. but may not.
     #
+    # TODO:  Integrate the testType() and findSEXPTypesFromSwitch() to determine 
+    # the types of the parameters with these additional guides.
     #
 function(p)
 {
     users = getAllUsers(p)
     k = sapply(users, getCallName)
+    k = k[!is.na(k)]  # will get NA from getCallName for a ReturnInst, i.e., when returning the parameter.
     ku = unique(k)
-    w = ku %in% c("REAL", "INTEGER", "LOGICAL", "STRING_ELT", "SET_STRING_ELT", "VECTOR_ELT", "SET_VECTOR_ELT")
+    w1 = ku %in% c("VECTOR_ELT", "SET_VECTOR_ELT")
+
+    # Can match more than one, e.g., third parameter in influence() which is a REAL and also inserted into a list as an element via SET_VECTOR_ELT()    
+    if(any(w1)) {
+        # See in what capacity
+    }
+    
+    w = ku %in% c("REAL", "INTEGER", "LOGICAL", "STRING_ELT", "SET_STRING_ELT")
+        
     ans = if(any(w)) {
         i = match(tolower(ku[w]), RSEXPTypeValues.df$rtypeName)
         if(!is.na(i))
@@ -36,15 +50,8 @@ function(p)
         return(ans)
     
 
-    if("R_ExternalPtrAddr" %in% ku)
+    if(all(ku %in% c("EXTPTR_PTR", "R_ExternalPtrAddr")))
         return(structure(list(type = "ExternalPtr"), class = "RExternalPtrType"))
-
-    
-    if(!is.na(k) && "TYPEOF" %in% k) {
-            tyu = getAllUsers(users[ k == "TYPEOF" ])
-            w = sapply(tyu, function(x) if(is(x, "ICmpInst")) lapply(x[], function(x) if(is(x, "ConstantInt")) getValue(x)))
-            ans = tyu[w]
-        }
 
 
     if(any(ku %in% "Rf_coerceVector")) {
@@ -66,6 +73,19 @@ function(p)
         return(structure(list(type = ty, length = 1), class = c("RScalarType", "RVectorType")))
     }
 
+     # Do we ever access this as an external ptr.
+    ext =  c("EXTPTR_PTR", "R_ExternalPtrAddr") %in% ku    
+
+    typeof = getTypeOf(p, users, k)
+    if(any(ext) && all(typeof == "externalptr"))
+        return(structure(list(type = "externalptr"), class = "RExternalPtr"))
+
+
+    isEnv = isUsedAsEnv(p, users, k)
+    if(all(isEnv) && (length(typeof) == 0 || "environment" %in% typeof))
+        return(structure(list(type = "environment"), class = "REnvironment"))
+
+    
     if(is.null(ans)) {
          #
          #  Look at where the argument was stored to a local variable
@@ -97,8 +117,15 @@ function(p)
         
     }
 
+
+    if(length(typeof))
+        return(structure(list(type = typeof)))
+    
     # 
     # We want ANY rather than NULL when we know it can be anything.
+
+    if(length(ans) == 0)
+       ans = structure(list("ANY"), class = "ANY")
 
     ans
 }
@@ -118,4 +145,58 @@ function(argNum, call)
         stop("Need to find ", fun, " and determine the types of its parameters")
     }
     
+}
+
+
+
+
+
+getTypeOf =
+    # Find the uses of TYPEOF(param)  and see what these values are compared to determine
+    # what types are expected.
+    #
+    #  want to find both positive comparisons and negative comparisons that lead to an error
+    #  e.g.
+    #   if(TYPEOF(param) != EXTPTRSXP)  error()    (see Cairo::raw_to_ptr, nseval::_dots_to_env)
+    # and
+    #    switch(TYPEOF(param)) { case INTSXP  ;  case REALSXP ; .....}
+    #
+    #
+function(p, users = getAllUsers(p), k = sapply(users, getCallName))
+{
+    if(!all(is.na(k)) && "TYPEOF" %in% k) {
+           # Was getAllUsers(users[ k == "TYPEOF" ] ) but that doesn't make sense as it is a list, not a single Value.
+        tyu = unlist(lapply(users[ !is.na(k) & k == "TYPEOF" ], getAllUsers)) 
+               # Looks flaky!
+        w = lapply(tyu, function(x)
+                            if(is(x, "ICmpInst")) {
+                                 sapply(x[], function(x)
+                                         if(is(x, "ConstantInt")) {
+                                             getValue(x)
+                                         } else
+                                            -1L
+                                        )
+                             } else
+                                -1L)
+
+
+        ans = unlist(w)
+        getRType(ans[ans > -1])
+    } else
+        integer()
+}
+
+
+
+isUsedAsEnv =
+    # Should this be isUsedOnlyAsEnv
+function(p, users = getAllUsers(p), k = sapply(users, getCallName))
+{
+    # R routines that have a parameter that is an environment.  Name -> parameter number
+    envirParamMap = c("Rf_defineVar" = 3L, Rf_findVar = 2L, Rf_findVarInFrame3 = 1L, findVarLocInFrame = 1L,
+                         findVarInFrame = 1L)
+    m = match(k, names(envirParamMap))
+    any(mapply( function(call, idx)
+                  identical(call[[3]], p),
+               users[!is.na(m)], envirParamMap[!is.na(m)]))
 }
